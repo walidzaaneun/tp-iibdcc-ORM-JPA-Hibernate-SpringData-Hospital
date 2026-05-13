ee2c9bc8642545c06c5689ba5cb885ef4b88522b
walidzaaneun <zaanoun3@gmail.com>
2026-05-13 23:51:42 +0100

    init project

---
+++ src/main/java/com/walidzaaneun/tpiibdccspringdatahospital/TpIibdccSpringDataHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.walidzaaneun.tpiibdccspringdatahospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIibdccSpringDataHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpIibdccSpringDataHospitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/walidzaaneun/tpiibdccspringdatahospital/TpIibdccSpringDataHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walidzaaneun.tpiibdccspringdatahospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIibdccSpringDataHospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
